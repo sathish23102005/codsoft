195acf5c5f618b3c819e7410f3a816fad94184cd
Sathish S <sathish23102005@gmail.com>
2025-07-12 22:52:41 -0700

    Create Number Game.java
    I created java program for number game 1 to 100

---
+++ Number Game.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+public static void main(string[]args){
+Random random = new Random();
+int randomnumber = random.nextInt(100) + 1;
+System.out.println("Random number between 1 and 100:" +randomNumber);
+}
+}
